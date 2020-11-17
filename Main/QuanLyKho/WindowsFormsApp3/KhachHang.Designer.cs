a448030809770d35c5b7fa3c378e4284589c8db0
hoangphu1607 <62040675+hoangphu1607@users.noreply.github.com>
2020-11-17 11:21:39 +0700

    design by phu
    phu dang code

--- Main/QuanLyKho/WindowsFormsApp3/KhachHang.Designer.cs
+++ Main/QuanLyKho/WindowsFormsApp3/KhachHang.Designer.cs
@@ -28,50 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgv_khachhang = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.txt_sdt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_ten = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_makh = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.MaKhachHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TenKhach = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.SDT = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txt_diachi = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_khachhang)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgv_khachhang
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgv_khachhang.AllowUserToAddRows = false;
+            this.dgv_khachhang.AllowUserToDeleteRows = false;
+            this.dgv_khachhang.AllowUserToResizeColumns = false;
+            this.dgv_khachhang.AllowUserToResizeRows = false;
+            this.dgv_khachhang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_khachhang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.MaKhachHang,
             this.TenKhach,
             this.SDT,
             this.DiaChi});
-            this.dataGridView1.Location = new System.Drawing.Point(379, 258);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(832, 591);
-            this.dataGridView1.TabIndex = 0;
+            this.dgv_khachhang.Location = new System.Drawing.Point(379, 258);
+            this.dgv_khachhang.Name = "dgv_khachhang";
+            this.dgv_khachhang.Size = new System.Drawing.Size(832, 591);
+            this.dgv_khachhang.TabIndex = 0;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.txt_diachi);
+            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.txt_sdt);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.txt_ten);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txt_makh);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(379, 25);
@@ -82,44 +89,21 @@
             this.groupBox1.Text = "Thêm Khách Hàng";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // label1
+            // label4
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Mã Khách Hàng";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(411, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Địa Chỉ";
             // 
-            // textBox1
+            // txt_sdt
             // 
-            this.textBox1.Location = new System.Drawing.Point(188, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(145, 26);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(552, 43);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(145, 26);
-            this.textBox2.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(411, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Tên Khách Hàng";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(188, 90);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(145, 26);
-            this.textBox3.TabIndex = 5;
+            this.txt_sdt.Location = new System.Drawing.Point(188, 90);
+            this.txt_sdt.Name = "txt_sdt";
+            this.txt_sdt.Size = new System.Drawing.Size(145, 26);
+            this.txt_sdt.TabIndex = 5;
             // 
             // label3
             // 
@@ -130,21 +114,38 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Số Điện Thoại";
             // 
-            // textBox4
+            // txt_ten
             // 
-            this.textBox4.Location = new System.Drawing.Point(552, 87);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(145, 26);
-            this.textBox4.TabIndex = 7;
+            this.txt_ten.Location = new System.Drawing.Point(552, 43);
+            this.txt_ten.Name = "txt_ten";
+            this.txt_ten.Size = new System.Drawing.Size(191, 26);
+            this.txt_ten.TabIndex = 3;
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(411, 90);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 20);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Địa Chỉ";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(411, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên Khách Hàng";
+            // 
+            // txt_makh
+            // 
+            this.txt_makh.Enabled = false;
+            this.txt_makh.Location = new System.Drawing.Point(188, 40);
+            this.txt_makh.Name = "txt_makh";
+            this.txt_makh.Size = new System.Drawing.Size(145, 26);
+            this.txt_makh.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Khách Hàng";
             // 
             // button1
             // 
@@ -154,16 +155,28 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Thêm";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(594, 218);
+            this.button2.Location = new System.Drawing.Point(1136, 218);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Liệt Kê";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::WindowsFormsApp3.Properties.Resources.daucong;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(351, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(32, 29);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // MaKhachHang
             // 
@@ -172,7 +185,7 @@
             this.MaKhachHang.HeaderText = "ID";
             this.MaKhachHang.Name = "MaKhachHang";
             this.MaKhachHang.ReadOnly = true;
-            this.MaKhachHang.Width = 70;
+            this.MaKhachHang.Width = 150;
             // 
             // TenKhach
             // 
@@ -181,7 +194,7 @@
             this.TenKhach.HeaderText = "Tên Khách Hàng";
             this.TenKhach.Name = "TenKhach";
             this.TenKhach.ReadOnly = true;
-            this.TenKhach.Width = 200;
+            this.TenKhach.Width = 300;
             // 
             // SDT
             // 
@@ -190,6 +203,7 @@
             this.SDT.HeaderText = "Số Điện Thoại";
             this.SDT.Name = "SDT";
             this.SDT.ReadOnly = true;
+            this.SDT.Width = 150;
             // 
             // DiaChi
             // 
@@ -198,7 +212,44 @@
             this.DiaChi.HeaderText = "Địa Chỉ";
             this.DiaChi.Name = "DiaChi";
             this.DiaChi.ReadOnly = true;
-            this.DiaChi.Width = 300;
+            this.DiaChi.Width = 200;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(567, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Xóa";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(870, 217);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Cập nhật ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txt_diachi
+            // 
+            this.txt_diachi.Location = new System.Drawing.Point(552, 87);
+            this.txt_diachi.Name = "txt_diachi";
+            this.txt_diachi.Size = new System.Drawing.Size(191, 26);
+            this.txt_diachi.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(730, 217);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "test";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // KhachHang
             // 
@@ -206,15 +257,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1675, 915);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgv_khachhang);
             this.Name = "KhachHang";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "KhachHang";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.KhachHang_FormClosed);
             this.Load += new System.EventHandler(this.KhachHang_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_khachhang)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -223,21 +278,25 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgv_khachhang;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txt_sdt;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txt_ten;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_makh;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridViewTextBoxColumn MaKhachHang;
         private System.Windows.Forms.DataGridViewTextBoxColumn TenKhach;
         private System.Windows.Forms.DataGridViewTextBoxColumn SDT;
         private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txt_diachi;
+        private System.Windows.Forms.Button button6;
     }
 }
--- Main/QuanLyKho/WindowsFormsApp3/KhachHang.cs
+++ Main/QuanLyKho/WindowsFormsApp3/KhachHang.cs
@@ -23,10 +23,15 @@ namespace WindowsFormsApp3
         //Connection conn = new Connection();
 
         SqlConnection conn= new SqlConnection(@"Data Source=DESKTOP-N15KRQ0\SQLEXPRESS;Initial Catalog=QLKho;Integrated Security=True");
-
+        
+        BindingNavigator a = new BindingNavigator();
+        SqlCommand comm;
+        string sql = "",moi = "",up="";
+        double number = 0;
+        int dem =0, somoi;
         private void KhachHang_Load(object sender, EventArgs e)
         {
-            //chay dc
+            conn.Open();
             
         }
 
@@ -40,9 +45,112 @@ namespace WindowsFormsApp3
             SqlDataAdapter dap = new SqlDataAdapter("SELECT * FROM KHACH_HANG", conn);
             DataTable table = new DataTable();
             dap.Fill(table);
-            dataGridView1.AutoGenerateColumns = false;
-            dataGridView1.DataSource = table;
+            //dgv_khachhang.AutoGenerateColumns = false;
+            dgv_khachhang.DataSource = table;
 
+        }
+
+        private void KhachHang_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgv_khachhang.Rows.Add(txt_makh.Text,txt_ten.Text,txt_sdt.Text,txt_diachi.Text);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            moi += "";
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();            
+            dem++;
+            if (dem == 1)
+            {                               
+                string sql = "Select * from MaKhachMoiNhat";
+                //tao 1 lenh command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //chay datareader
+                SqlDataReader drd = cmd.ExecuteReader();
+                string kq = "";
+                while (drd.Read())
+                {
+                    kq += drd["MaKhachHang"].ToString();
+                }
+                //chuyen cac so cuoi thanh so
+                number = Convert.ToDouble(kq.Substring(4));                
+                //doi number thanh so            
+                txt_makh.Text = "KHTN" + (number + 1);
+                conn.Close();
+                moi = "KHTN" + (number + 1);
+            }
+            else
+            {                              
+                txt_makh.Text = "KHTN" + (number + dem);
+                moi = "KHTN" + (number + dem);
+                conn.Close();
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            foreach (DataGridViewRow item in this.dgv_khachhang.SelectedRows)
+            {
+                dgv_khachhang.Rows.RemoveAt(item.Index);
+            }
+            conn.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string makh = "", tenkhach = "", sdt = "", diachi = "";
+            makh += dgv_khachhang.Rows[0].Cells["MaKhachHang"].Value;
+            tenkhach += dgv_khachhang.Rows[0].Cells["TenKhach"].Value;
+            sdt += dgv_khachhang.Rows[0].Cells["SDT"].Value;
+            diachi += dgv_khachhang.Rows[0].Cells["Diachi"].Value;
+
+            string sql = @"INSERT INTO KHACH_HANG VALUES ('" + makh + @"', N''" + tenkhach + @"', '" + sdt + @"', N''" + diachi + @"');";
+
+            MessageBox.Show(sql);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            for (int i = 0; i < dgv_khachhang.Rows.Count; i++)
+            {
+                string makh = "", tenkhach = "", sdt = "", diachi = "";
+                makh += dgv_khachhang.Rows[i].Cells["MaKhachHang"].Value;
+                tenkhach += dgv_khachhang.Rows[i].Cells["TenKhach"].Value;
+                sdt += dgv_khachhang.Rows[i].Cells["SDT"].Value;
+                diachi += dgv_khachhang.Rows[i].Cells["Diachi"].Value;
+
+                string sql = "INSERT INTO KHACH_HANG VALUES ('" + makh + "', N'" + tenkhach + "', N'" + diachi  +"','"+ sdt  + "');";
+                comm = new SqlCommand(sql, conn);
+                comm.ExecuteNonQuery();
+
+                if(i == dgv_khachhang.Rows.Count)
+                {
+                    up = "UPDATE MaKhachMoiNhat SET MaKhachHang = "+ moi; 
+                }    
+            }
+            conn.Close();
+
+            txt_makh.Clear();
+            txt_ten.Clear();
+            txt_sdt.Clear();
+            txt_sdt.Clear();
+            //if(i == dgv_khachhang.Rows.Count - 1)
+            //{
+            //    up = @"UPDATE" 
+            //}
         }
     }
 }
